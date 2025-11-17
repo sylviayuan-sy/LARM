e11c6eff89ade484482030a2e055143c809832ba
sylviayuan-sy <l1yuan@ucsd.edu>
2025-11-17 00:01:58 -0800

    rm scripts

--- scripts/run_part.sh
+++
@@ -1,9 +0,0 @@
-export MASTER_ADDR=127.0.0.1
-export MASTER_PORT=6006
-export JOB_COMPLETION_INDEX=0
-NCCL_DEBUG=INFO torchrun --nproc_per_node 2 --nnodes 1 \
-    --master-port ${MASTER_PORT} \
-    --master-addr ${MASTER_ADDR} \
-    --node-rank ${JOB_COMPLETION_INDEX} \
-    trainer_mask.py \
-    --config=configs/part.yaml 
